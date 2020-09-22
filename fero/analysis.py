81634e2ab9966e0e0e7467e9b5e6c8f95dda926e
William Brinkerhoff <will@ferolabs.com>
2020-09-22 14:35:54 -0400

    added get analysis method

---
+++ fero/analysis.py
@@ -0,0 +1,62 @@
+from marshmallow import Schema, fields
+
+
+class AnalysisSchema(Schema):
+
+    url = fields.String(required=True)
+
+    predictions_url = fields.String(required=True)
+    revisions_url = fields.String(required=True)
+
+    created_by = fields.Dict(required=True)
+
+    data_source_name = fields.String(required=True)
+
+    data_source_deleted = fields.Boolean()
+
+    latest_revision = fields.Integer()
+
+    latest_revision_model = fields.UUID(required=True)
+
+    latest_revision_model_state = fields.String(required=True)
+
+    latest_trained_revision = fields.Integer(required=True, allow_none=True)
+
+    latest_trained_revision_model = fields.UUID(allow_none=True)
+
+    latest_completed_model = fields.UUID(allow_none=True)
+
+    latest_completed_model_score = fields.Integer(required=True, allow_none=True)
+
+    latest_completed_model_score_qualifier = fields.String(
+        required=True, allow_none=True
+    )
+
+    latest_completed_model_modified = fields.DateTime(required=True, allow_none=True)
+
+    schema_overrides = fields.Dict(default=dict, missing=dict)
+
+    display_options = fields.Dict(allow_none=True, default=dict, missing=dict)
+    ac_name = fields.String(required=True)
+
+    uuid = fields.UUID(required=True)
+    name = fields.String(required=True)
+    data_source = fields.UUID(required=True)
+    created = fields.DateTime(require=True)
+    modified = fields.DateTime(require=True)
+    blueprint_name = fields.String(required=True)
+
+
+class Analysis:
+    def __init__(self, client: "fero.Fero", data: dict):
+        self._client = client
+        schema = AnalysisSchema()
+        self._data = schema.load(data)
+
+    def __getattr__(self, name: str):
+        return self._data.get(name)
+
+    def __repr__(self):
+        return f"<Analysis name={self.name}>"
+
+    __str__ = __repr__
