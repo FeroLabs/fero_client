baca2e7d8272abc14e8a649b2706f3e3a669ba2f
William Brinkerhoff <will@ferolabs.com>
2020-09-24 14:19:41 -0400

    bumped version since pypi can't replace

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="fero",
-    version="1.0.0",
+    version="1.1.0",
     description="Python client for accessing Fero API",
     long_description=long_description,
     long_description_content_type="text/markdown",
