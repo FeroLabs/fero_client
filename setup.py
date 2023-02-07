bb68362a20f2792cf1ede24ce1e3c0b07d4aae2c
Luke Grecki <luke@ferolabs.com>
2023-02-07 09:10:35 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 long_description = (here / "README.md").read_text(encoding="utf-8")
-VERSION = "2.1.6"
+VERSION = "2.1.7"
 
 
 class VerifyVersionCommand(install):
